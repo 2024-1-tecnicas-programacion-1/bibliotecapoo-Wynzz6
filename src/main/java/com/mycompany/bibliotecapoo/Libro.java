8974248efb8b1ee1d0b401f1fe72fc3383944a9c
Wynzz6 <141454169+Wynzz6@users.noreply.github.com>
2024-05-01 00:19:37 -0500

    Clase Libro hecha

--- src/main/java/com/mycompany/bibliotecapoo/Libro.java
+++ src/main/java/com/mycompany/bibliotecapoo/Libro.java
@@ -1,34 +1,43 @@
 package com.mycompany.bibliotecapoo;
 
 public class Libro {
-     String titulo;
+    String titulo;
     String autor;
     int anoPublicacion;
     boolean leido;
     String genero;
 
-
     public Libro(String titulo, String autor, int anoPublicacion, boolean leido, String genero){
         this.titulo = titulo;
         this.autor = autor;
         this.anoPublicacion = anoPublicacion;
-        this.leido = false; 
+        this.leido = leido; 
         this.genero = genero;
-
     }
 
     public String mostrarInformacion(){
         return "Titulo " + titulo + ", " + "Autor " + autor + ", " + "Ano de publicacion " + anoPublicacion + ", " + "Genero " + genero;
     }
 
-  public String marcarLeido() {
-    this.leido = true;
-    return "El libro ya fue leído.";
-}
+    public boolean marcarLeido() {
+        if (!leido) {
+            leido = true;
+            return true;
+        } else {
+            return false; // El libro ya estaba marcado como leído
+        }
+    }
+
+    public boolean esLeido() {
+        return leido;
+    }
 
     public boolean esAntiguo() {
-    int anoActual = 2024;
-    return anoActual - anoPublicacion > 50;
+        int anoActual = 2024;
+        return anoActual - anoPublicacion > 50;
+    }
+     public String getTitulo() {
+        return titulo;
 }
 }
   
