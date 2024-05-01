f31008e66a4ee47ca64a47f2c4f9378e42ab59fd
Wynzz6 <141454169+Wynzz6@users.noreply.github.com>
2024-05-01 00:20:34 -0500

    Clase biblioteca hecha

--- src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
+++ src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
@@ -3,12 +3,37 @@ package com.mycompany.bibliotecapoo;
 import java.util.LinkedList;
 
 public class Biblioteca {
-  private LinkedList<Libro> libros;
-  
-  public Biblioteca() {
-      this.libros = new Linkedlist<>();
-      
-  }
-  
-  }
+    private LinkedList<Libro> libros;
 
+    public Biblioteca() {
+        this.libros = new LinkedList<>();
+    }
+
+    public void registrarLibro(Libro libro) {
+        libros.add(libro);
+    }
+
+    public void mostrarLibros() {
+        for (Libro libro : libros) {
+            System.out.println(libro.mostrarInformacion());
+        }
+    }
+
+    public void mostrarLibrosNoLeidos() {
+        for (Libro libro : libros) {
+            if (libro.marcarLeido()) {
+            } else {
+                System.out.println(libro.mostrarInformacion());
+            }
+        }
+    }
+
+    public Libro buscarLibro(String palabraBusqueda) {
+        for (Libro libro : libros) {
+            if (libro.getTitulo().toLowerCase().contains(palabraBusqueda.toLowerCase())) {
+                return libro;
+            }
+        }
+        return null;
+    }
+}
